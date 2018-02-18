eec41f81be86276b0f7733b52d6c8ac6c18a3d2f
Rangel Varnier <rangel.varnier@gmail.com>
2018-02-18 10:24:41 -0300

    adicionado express como server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
-console.log('Ola Mundo!');
+import * as express from 'express';
 
-console.log('Ola!');
+class App {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.midleware();
+  }
+
+  private midleware(): void {
+    this.express.use(
+      '/hello',
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
+      ) => {
+        res.send({
+          hello: 'Hello World',
+        });
+      },
+    );
+  }
+}
+
+export default new App().express;
