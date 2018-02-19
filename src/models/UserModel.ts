185f91ac41574301ab5d1e6b55061f19396cc033
Rangel Varnier <rangel.varnier@gmail.com>
2018-02-18 22:36:16 -0300

    user model sequelize

---
+++ src/models/UserModel.ts
@@ -0,0 +1,86 @@
+import * as Sequelize from 'sequelize';
+import { genSalt, hashSync, compareSync, genSaltSync } from 'bcryptjs';
+import { BaseModelInterface } from '../interfaces/BaseModelInterface';
+import { ModelsInterface } from '../interfaces/ModeslInterface';
+
+export interface UserAttributes {
+  id?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  photo?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface UserInstance
+  extends Sequelize.Instance<UserAttributes>,
+    UserAttributes {
+  isPassword(ancodedPassword: string, password: string): boolean;
+}
+
+export interface UserModel
+  extends BaseModelInterface,
+    Sequelize.Model<UserInstance, UserAttributes> {}
+
+export default (
+  sequelize: Sequelize.Sequelize,
+  DataTypes: Sequelize.DataTypes,
+): UserModel => {
+  const User: UserModel = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      photo: {
+        type: DataTypes.BLOB({
+          length: 'long',
+        }),
+        allowNull: true,
+        defaultValue: null,
+      },
+    },
+    {
+      tableName: 'users',
+      hooks: {
+        beforeCreate: (
+          user: UserInstance,
+          options: Sequelize.CreateOptions,
+        ): void => {
+          const salt = genSaltSync();
+          user.password = hashSync(user.password, salt);
+        },
+      },
+    },
+  );
+
+  User.associate = (models: ModelsInterface): void => {};
+
+  User.prototype.isPassword = (
+    ancodedPassword: string,
+    password: string,
+  ): boolean => {
+    return compareSync(password, ancodedPassword);
+  };
+  return User;
+};
