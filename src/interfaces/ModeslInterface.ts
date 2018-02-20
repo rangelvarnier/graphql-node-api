ec9cb5264df98f076a8ddf4c79cc43ac5f0467fa
Rangel Varnier <rangel.varnier@gmail.com>
2018-02-19 22:52:23 -0300

    Comment, Post models

--- src/interfaces/ModeslInterface.ts
+++ src/interfaces/ModeslInterface.ts
@@ -1,1 +1,9 @@
-export interface ModelsInterface {}
+import { UserModel } from '../models/UserModel';
+import { PostModel } from '../models/PostModel';
+import { CommentModel } from '../models/CommentModel';
+
+export interface ModelsInterface {
+  User: UserModel;
+  Post: PostModel;
+  Comment: CommentModel;
+}
